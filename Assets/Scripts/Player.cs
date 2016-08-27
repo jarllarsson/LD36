aa54e1862d836c5d1e988f018655d7a8904ac56b
Jarl Larsson <jarl.larsson@gmail.com>
2016-08-27 13:14:34 +0200

    wall collision wip

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,9 +7,10 @@ public class Player : MonoBehaviour
     public Rigidbody2D rbody;
     public Collider2D collider;
 
-    public bool canMove = true;
+    //public bool canMove = true;
     public float downCheckLen = 1.0f;
-    public float sideCheckLen = 1.0f;
+    public float downCheckOffsetLen = 1.0f;
+    public float wallCheckLen = 1.0f;
 
     private int playerLayer = 0;
     private int platformLayer = 0;
@@ -17,6 +18,7 @@ public class Player : MonoBehaviour
     public Renderer myRenderer;
 
     public float jumpPwr = 100.0f;
+    public float instantJumpV = 3.0f;
     float jumpCooldown = 0.4f;
     float jumpCooldownCount = 0.0f;
     public bool onGround = false;
@@ -25,12 +27,14 @@ public class Player : MonoBehaviour
     private bool jumpTrigTapOn = false;
     private Vector2 jumpTrigDelta;
     private Vector3 jumpVec;
+    private Vector2 oldRStickDir;
 
     public Camera mainCam;
+    bool canMove = true;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         playerLayer = LayerMask.NameToLayer("Player");
         platformLayer = LayerMask.NameToLayer("Platforms");
         worldLayer = LayerMask.NameToLayer("World");
@@ -40,12 +44,21 @@ public class Player : MonoBehaviour
 	void FixedUpdate ()
     {
         float dir = Input.GetAxis("Horizontal");
+        Vector2 rStickDir = new Vector2(Input.GetAxis("RHorizontal"), Input.GetAxis("RVertical"));
+        float rStickMagnitude = rStickDir.magnitude;
+        if (rStickMagnitude >= 0.2f)
+            oldRStickDir = rStickDir;
         float jump = Input.GetAxis("Jump");
-        canMove = false;
+        //canMove = false;
         Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        bool jumpPress = Input.GetAxis("Fire1") > 0.0f;
 
-        if (Input.GetAxis("Fire1") > 0.0f)
+        if (jumpPress || rStickMagnitude > 0.1f)
         {
+            if (jumpPress)
+            {
+                jumpVec = Vector2.down;
+            }
             Debug.Log(mousePos);
             if (jumpTrigTapOn == false)
             {
@@ -60,15 +73,22 @@ public class Player : MonoBehaviour
                 Vector3 endWorldPos = mainCam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, transform.position.z - mainCam.transform.position.z));
                 Vector3 startWorldPos = mainCam.ScreenToWorldPoint(new Vector3(jumpTrigStart.x, jumpTrigStart.y, transform.position.z - mainCam.transform.position.z));
                 Vector3 deltaWorld = endWorldPos - startWorldPos;
-                jumpVec = deltaWorld.normalized;
+                if (deltaWorld.normalized.sqrMagnitude > 0.1f)
+                    jumpVec = deltaWorld.normalized;
                 //Debug.DrawLine(transform.position, transform.position + deltaWorld, Color.yellow);
                 Debug.DrawLine(startWorldPos, endWorldPos, Color.red);
+                Debug.DrawLine(transform.position, transform.position + new Vector3(oldRStickDir.x, oldRStickDir.y,0.0f), Color.red);
             }
         }
         else if (jumpTrigTapOn)
         {
+            if (oldRStickDir.sqrMagnitude > jumpVec.sqrMagnitude * 0.1f)
+            {
+                jumpVec = oldRStickDir.normalized;
+            }
             jumpTrigTapOn = false;
             Debug.DrawLine(transform.position, transform.position + new Vector3(jumpTrigDelta.x, jumpTrigDelta.y, 0.0f), Color.white, 2.0f);
+            Debug.DrawLine(transform.position, transform.position + new Vector3(oldRStickDir.x, oldRStickDir.y, 0.0f) * jumpPwr, Color.white, 2.0f);
             jump = 1.0f;
         }
 
@@ -76,13 +96,15 @@ public class Player : MonoBehaviour
         jumpCooldownCount -= Time.deltaTime;
         if (jump > 0.0f && jumpCooldownCount < 0.0f && rbody.velocity.y <= 0.01f/* && onGround*/)
         {
-            canMove = false;
+            //canMove = false;
+            rbody.velocity = new Vector2(rbody.velocity.x, Mathf.Max(rbody.velocity.y, -jumpVec.y * instantJumpV));
             rbody.AddForce(-jumpVec * jumpPwr);
             jumpCooldownCount = jumpCooldown;
+            oldRStickDir = Vector2.zero;
         }
         if (rbody.velocity.y <= 0.0f)
         {
-            canMove = true;
+            //canMove = true;
             myRenderer.material.color = Color.red;
         }
         else
@@ -103,17 +125,65 @@ public class Player : MonoBehaviour
         onGround = false;
         for (int i = -1; i < 2; i++)
         {
-            Debug.DrawLine(transform.position - Vector3.right * (float)i * sideCheckLen, transform.position - Vector3.up * downCheckLen - Vector3.right * (float)i * sideCheckLen);
-            Vector2 offset = -Vector3.right * (float)i * sideCheckLen;
+            Debug.DrawLine(transform.position - Vector3.right * (float)i * downCheckOffsetLen, transform.position - Vector3.up * downCheckLen - Vector3.right * (float)i * downCheckOffsetLen);
+            Vector2 offset = -Vector3.right * (float)i * downCheckOffsetLen;
             RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y) + offset, -Vector2.up, downCheckLen, ~(1 << playerLayer));
             if (hit.collider != null)
             {
-                if (hit.normal.y > hit.normal.x)
+                if (hit.normal.y > 0.0f && hit.normal.y > hit.normal.x)
                 {
                     onGround = true;
                 }
             }
         }
+
+        // wall check
+        /*for (int i = -1; i < 2; i++)
+        {
+            Debug.DrawLine(transform.position, transform.position - Vector3.right * (float)i * wallCheckLen);
+            RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.right * (float)i , wallCheckLen, ~(1 << playerLayer));
+            if (hit.collider != null)
+            {
+                if (Mathf.Abs(hit.normal.x) > Mathf.Abs(hit.normal.y))
+                {
+                    //canMove = false;
+                    rbody.velocity = new Vector2(0.0f, rbody.velocity.y);
+                }
+            }
+        }*/
     }
 
+    void OnCollisionEnter2D(Collision2D in_collision)
+    {
+        Vector2 avgNormal = averageHitNormal(in_collision);
+        HandleWallBounce(avgNormal);
+    }
+
+    void OnCollisionStay2D(Collision2D in_collision)
+    {
+        Vector2 avgNormal = averageHitNormal(in_collision);
+        HandleWallBounce(avgNormal);
+    }
+
+    void HandleWallBounce(Vector2 in_normal)
+    {
+        if (Mathf.Abs(in_normal.x) > Mathf.Abs(in_normal.y))
+        {
+            rbody.velocity = new Vector2(in_normal.x * moveSpd * 10.0f, rbody.velocity.y);
+        }
+    }
+
+    Vector2 averageHitNormal(Collision2D in_collision)
+    {
+        Vector2 normal = Vector2.zero;
+        int count = 0;
+        foreach (ContactPoint2D contact in in_collision.contacts)
+        {
+            //Debug.DrawRay(contact.point, contact.normal, Color.white, 1.0f);
+            count++;
+            normal += contact.normal;
+        }
+        if (count > 0) normal /= (float)count;
+        return normal;
+    }
 }
