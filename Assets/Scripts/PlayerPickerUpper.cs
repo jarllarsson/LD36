64519ff975899d7d050a15f3a0acc22900f2fa4f
Jarl <jarl.larsson@gmail.com>
2016-08-27 21:46:37 +0200

    pick up and then shoot (toggle)

--- Assets/Scripts/PlayerPickerUpper.cs
+++ Assets/Scripts/PlayerPickerUpper.cs
@@ -3,69 +3,75 @@ using System.Collections;
 
 public class PlayerPickerUpper : MonoBehaviour 
 {
-    public Player player;
-    public Collider2D pickUpArea;
-    public GameObject currentMonster;
-    Pickupable pickup;
-    public bool isActive = false;
+	public Player player;
+	public Collider2D pickUpArea;
+	public GameObject currentMonster;
+	Pickupable pickup;
+	public bool isAreaActive = false;
+    private bool toggle = false;
 
 	// Use this for initialization
 	void Start () 
-    {
+	{
 	
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () 
-    {
-        float fire = Input.GetAxis("Fire2");
-        if (fire > 0.0f)
-        {
-            if (!currentMonster)
+	{
+		float fire = Input.GetAxis("Fire2");
+		if (fire > 0.0f) // fire btn
+		{
+			if (!currentMonster) // if holding none
+			{
+				isAreaActive = true; // activate suction
+			}
+			else if (!pickup) // if holding but have not registered it
+			{
+				isAreaActive = false;
+				pickup = currentMonster.GetComponent<Pickupable>();
+				if (pickup && player && player.pointer)
+				{
+					pickup.pickupObj(player.pointer, Vector3.up); // trigger pickup, makes us own monster
+				}
+			}
+            else if (!toggle) // if holding, and registered and first button press after release
             {
-                isActive = true;
+			    currentMonster = null;
+			    if (pickup) pickup.shoot(Vector2.up * 100.0f); // fire away monster and reset
+			    pickup = null;
             }
-            else if (!pickup)
-            {
-                isActive = false;
-                pickup = currentMonster.GetComponent<Pickupable>();
-                if (pickup && player && player.pointer)
-                {
-                    pickup.pickupObj(player.pointer, Vector3.up);
-                }
-            }
-        }
-        else
-        {
-            isActive = false;
-            currentMonster = null;
-            if (pickup) pickup.shoot(Vector2.up * 100.0f);
-            pickup = null;
-        }
+            toggle = true;
+		}
+		else
+		{
+            toggle = false;
+			isAreaActive = false;
+		}
 	}
 
-    void OnTriggerEnter2D(Collider2D in_collider)
-    {
-        if (isActive && in_collider && in_collider.gameObject)
-        {
-            currentMonster = in_collider.gameObject;
-        }
-    }
+	void OnTriggerEnter2D(Collider2D in_collider)
+	{
+		if (isAreaActive && in_collider && in_collider.gameObject)
+		{
+			currentMonster = in_collider.gameObject;
+		}
+	}
 
-    void OnTriggerStay2D(Collider2D in_collider)
-    {
-        if (isActive && in_collider && in_collider.gameObject)
-        {
-            currentMonster = in_collider.gameObject;
-        }
-    }
+	void OnTriggerStay2D(Collider2D in_collider)
+	{
+		if (isAreaActive && in_collider && in_collider.gameObject)
+		{
+			currentMonster = in_collider.gameObject;
+		}
+	}
 
-    void OnTriggerExit2D(Collider2D in_collider)
-    {
-        if (in_collider && in_collider.gameObject && in_collider.gameObject == currentMonster)
-        {
-            currentMonster = null;
-        }
-    }
+	void OnTriggerExit2D(Collider2D in_collider)
+	{
+		if (in_collider && in_collider.gameObject && in_collider.gameObject == currentMonster)
+		{
+			currentMonster = null;
+		}
+	}
 
 }
